e221932b507f4220bb468138edc4f05147d2b4fa
faker <test@gmail.com>
2025-04-25 23:30:21 +0700

    Wed

---
+++ De xe co js menu/All/Resource/js/mycode.js
@@ -0,0 +1,10 @@
+// js/mycode.js
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
